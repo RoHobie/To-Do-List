efa381d8238ddb0fce4241ad2289671605f3768a
Ronit Thakur <74425419+RoHobie@users.noreply.github.com>
2025-01-09 14:30:45 +0530

    Update script.js
    Uses an array to store the tasks and their status

--- script.js
+++ script.js
@@ -1,24 +1,81 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
-function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something");
+const todoForm = document.querySelector('form');
+const todoInput = document.getElementById('input-box');
+const todoListUL = document.getElementById('to-do-list');
+
+let allTodos = getTodos();
+updateTodoList();
+
+todoForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    addTodo();
+})
+
+function addTodo() {
+    const todoText = todoInput.value.trim();
+    if (todoText.length > 0) {
+        const todoObject = {
+            text: todoText,
+            isDone: false
+        }
+        allTodos.push(todoObject);
+        updateTodoList();
+        todoInput.value = "";
+        saveTodos();
     }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = '';
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-    }else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-    }
-}, false);
+function updateTodoList() {
+    todoListUL.innerHTML = "";
+    allTodos.forEach((todo, todoIndex) => {
+        todoItem = createTodoItem(todo, todoIndex);
+        todoListUL.append(todoItem);
+    })
+}
+function createTodoItem(todo, todoIndex) {
+    const todoId = "todo-" + todoIndex;
+    const todoLI = document.createElement("li");
+    const todoText = todo.text;
+    todoLI.className = "todo";
+    todoLI.innerHTML = `
+                <input type="checkbox" id="${todoId}">
+                <label class="custom-checkbox" for="${todoId}">
+                    <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+                    <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+                </label>
+                <label class="to-do-text" for="${todoId}">
+                ${todoText}
+                </label>
+                <button class="delete-button">
+                    <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+                        <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                    </button>
+    `
+    const deleteButton = todoLI.querySelector(".delete-button");
+    deleteButton.addEventListener("click", ()=>{
+        deleteTodoItem(todoIndex);
+    })
+    const checkbox = todoLI.querySelector("input");
+    checkbox.addEventListener("change", ()=>{
+        allTodos[todoIndex].isDone= checkbox.checked;
+        saveTodos();
+    })
+    checkbox.checked = todo.isDone;
+    return todoLI;
+}
+
+function saveTodos(){
+    const todoJson = JSON.stringify(allTodos);
+    localStorage.setItem("todos",todoJson);
+}
+
+function getTodos(){
+    const todos = localStorage.getItem("todos") || "[]";
+    return JSON.parse(todos);
+}
+
+function deleteTodoItem(todoIndex){
+    allTodos = allTodos.filter((_,i)=>i !== todoIndex);
+    saveTodos();
+    updateTodoList();
+}
+
